8a49a175053c3565cc682b01a58236bbbc66c140
strong-minsu <dlalstn5789@gmail.com>
2022-10-20 17:20:22 +0900

    hi

--- src/pages/ListBoard.jsx
+++ src/pages/ListBoard.jsx
@@ -20,39 +20,39 @@ class ListBoard extends Component {
     // # 3.
     render() {
         return (
-            <div>
-                <h2 className="text-center">Boards List</h2>
-                <div className ="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>글 번호</th>
-                                <th>타이틀 </th>
-                                <th>작성자 </th>
-                                <th>작성일 </th>
-                                <th>갱신일 </th>
-                                <th>좋아요수</th>
-                                <th>조회수</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.boards.map(
-                                    board => 
-                                    <tr key = {board.no}>
-                                        <td> {board.no} </td>
-                                        <td> {board.title} </td>
-                                        <td> {board.memberNo} </td>
-                                        <td> {board.createdTime} </td>
-                                        <td> {board.updatedTime} </td>
-                                        <td> {board.likes} </td>
-                                        <td> {board.counts} </td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                </div>
+          <div>
+            <h2 className="text-center">Boards List</h2>
+                 <div className ="row">
+                     <table className="table table-striped table-bordered">
+                         <thead>
+                             <tr>
+                                 <th>글 번호</th>
+                                 <th>타이틀 </th>
+                                 <th>작성자 </th>
+                                 <th>작성일 </th>
+                                 <th>갱신일 </th>
+                                 <th>좋아요수</th>
+                                 <th>조회수</th>
+                             </tr>
+                         </thead>
+                         <tbody>
+                             {
+                                 this.state.boards.map(
+                                     board => 
+                                     <tr key = {board.no}>
+                                         <td> {board.no} </td>
+                                         <td> {board.title} </td>
+                                         <td> {board.memberNo} </td>
+                                         <td> {board.createdTime} </td>
+                                         <td> {board.updatedTime} </td>
+                                         <td> {board.likes} </td>
+                                         <td> {board.counts} </td>
+                                     </tr>
+                                 )
+                                 }
+                         </tbody>
+                     </table>
+          </div>
         );
     }
 }
